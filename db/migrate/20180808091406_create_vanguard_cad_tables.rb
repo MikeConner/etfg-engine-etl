291e24a59e23dc0cc2e13218ad70eff8d069681a
birdylockstep <bird@locksteplabs.com>
2018-08-08 20:10:10 +0700

    Vanguard CAD migrations

---
+++ db/migrate/20180808091406_create_vanguard_cad_tables.rb
@@ -0,0 +1,68 @@
+class CreateVanguardCadTables < ActiveRecord::Migration[5.2]
+  def change
+    create_table :vanguard_cad_all_fund_holdings do |t|
+      t.string :fund_ticker
+      t.string :fund_name
+      t.integer :fund_id
+      t.date :as_of_date
+      t.integer :equity_holdings_count
+      t.integer :bond_holdings_count
+      t.integer :short_term_reserve_count
+      t.string :listed_currency
+      t.string :listed_currency_symbol
+      t.string :fund_currency
+      t.string :fund_currency_symbol
+      t.string :asset_type
+      t.string :holding_name
+      t.string :ticker
+      t.string :cusip
+      t.string :sedol
+      t.string :isin
+      t.decimal :percent_of_fund
+      t.string :sector
+      t.string :country
+      t.string :security_depository_receipt_type
+      t.integer :market_value
+      t.integer :face_amount
+      t.decimal :coupon_rate
+      t.date :maturity_date
+      t.integer :shares
+      t.string :currency_code
+      t.string :currency_symbol
+      t.date :etfg_date
+    end
+
+    create_table :vanguard_cad_etfnav do |t|
+      t.string :ticker
+      t.string :isin
+      t.string :sedol
+      t.string :cusip
+      t.string :description
+      t.date :trade_date
+      t.decimal :nav
+      t.string :status
+      t.integer :shares_in_creation_units
+      t.integer :application_value
+      t.decimal :sum_of_market_value
+      t.decimal :actualcash
+      t.date :etfg_date
+    end
+
+    create_table :vanguard_cad_fi_etfnav do |t|
+      t.string :ticker
+      t.string :isin
+      t.string :sedol
+      t.string :cusip
+      t.string :description
+      t.string :basket_type
+      t.date :trade_date
+      t.decimal :nav
+      t.string :status
+      t.integer :shares_in_creation_units
+      t.integer :application_value
+      t.decimal :sum_of_market_value
+      t.decimal :actualcash
+      t.date :etfg_date
+    end
+  end
+end
