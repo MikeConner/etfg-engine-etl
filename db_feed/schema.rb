563c9173e75c22f58af16dd21d7324586183642c
birdylockstep <bird@locksteplabs.com>
2018-08-17 16:27:39 +0700

    Vanguard US migrations

--- db_feed/schema.rb
+++ db_feed/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_043408) do
+ActiveRecord::Schema.define(version: 2018_08_17_063724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -200,6 +200,72 @@ ActiveRecord::Schema.define(version: 2018_08_16_043408) do
     t.index ["composite_ticker"], name: "index_staging_constituents_on_composite_ticker"
     t.index ["etfg_date", "datasource_id"], name: "index_staging_constituents_on_etfg_date_and_datasource_id"
     t.index ["figi"], name: "index_staging_constituents_on_figi"
+  end
+
+  create_table "vanguard_us_all_fund_holdings", force: :cascade do |t|
+    t.string "fund_ticker"
+    t.string "fund_name"
+    t.integer "fund_id"
+    t.date "as_of_date"
+    t.integer "equity_holdings_count"
+    t.integer "bond_holdings_count"
+    t.integer "short_term_reserve_count"
+    t.string "listed_currency"
+    t.string "listed_currency_symbol"
+    t.string "fund_currency"
+    t.string "fund_currency_symbol"
+    t.string "asset_type"
+    t.string "holding_name"
+    t.string "ticker"
+    t.string "cusip"
+    t.string "sedol"
+    t.string "isin"
+    t.decimal "percent_of_fund"
+    t.string "sector"
+    t.string "country"
+    t.string "security_depository_receipt_type"
+    t.bigint "market_value"
+    t.bigint "face_amount"
+    t.decimal "coupon_rate"
+    t.date "maturity_date_start"
+    t.date "maturity_date_end"
+    t.bigint "shares"
+    t.string "currency_code"
+    t.string "currency_symbol"
+    t.date "etfg_date"
+  end
+
+  create_table "vanguard_us_etfnav", force: :cascade do |t|
+    t.string "ticker"
+    t.string "isin"
+    t.string "sedol"
+    t.string "cusip"
+    t.string "description"
+    t.date "trade_date"
+    t.decimal "nav"
+    t.string "status"
+    t.integer "shares_in_creation_units"
+    t.integer "application_value"
+    t.decimal "sum_of_market_value"
+    t.decimal "actualcash"
+    t.date "etfg_date"
+  end
+
+  create_table "vanguard_us_fi_etfnav", force: :cascade do |t|
+    t.string "ticker"
+    t.string "isin"
+    t.string "sedol"
+    t.string "cusip"
+    t.string "description"
+    t.string "basket_type"
+    t.date "trade_date"
+    t.decimal "nav"
+    t.string "status"
+    t.integer "shares_in_creation_units"
+    t.integer "application_value"
+    t.decimal "sum_of_market_value"
+    t.decimal "actualcash"
+    t.date "etfg_date"
   end
 
 end
