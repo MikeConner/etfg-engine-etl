d3703a7603a934130f4a2aebc97e5299ef9c63eb
Jeff <endymionjkb@gmail.com>
2018-09-12 19:21:46 -0400

    Tolerate leading , and CR in Vanguard Holdings file

--- lib/kiba/sources/vanguard_holdings_source.rb
+++ lib/kiba/sources/vanguard_holdings_source.rb
@@ -4,12 +4,13 @@ require 'csv'
 class VanguardHoldingsSource
   def initialize(file)
     @file = file
+    `perl -p -i -e 's/\r\n$/\n/g' #{file}`
   end
 
   # Yield each row, including blanks for transitions; they are all different lengths
   def each
     CSV.foreach(@file, :encoding => 'iso-8859-1:utf-8') do |row|
-      yield row
+      yield row[0].blank? ? row[1, row.length] : row
     end
   end
 end
